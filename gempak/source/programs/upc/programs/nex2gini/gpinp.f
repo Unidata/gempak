67254da5a8dd2d43dad5ca9c7bb2ea8e51c26ae9
mjames-upc <mjamesis@gmail.com>
2013-12-05 15:48:52 -0700

    cleaning up nex2gini fix

--- gempak/source/programs/upc/programs/nex2gini/gpinp.f
+++ gempak/source/programs/upc/programs/nex2gini/gpinp.f
@@ -1,5 +1,5 @@
 	SUBROUTINE GPINP  ( proj, gdarea, kxky, gfunc, satfil, radtim, 
-     +                      raddur, radfrq, cpyfil, stnfil, radmode,
+     +                      raddur, radfrq, cpyfil, stnfil, 
      +			    compress, iret )
 C************************************************************************
 C* GPINP								*
@@ -13,7 +13,7 @@ C* Log:									*
 C* Chiz/Unidata	 3/01	Developed from GPMAP				*
 C************************************************************************
 	CHARACTER*(*)	proj, gdarea, kxky, gfunc, satfil, radtim,
-     +			raddur, radfrq, cpyfil, stnfil, radmode
+     +			raddur, radfrq, cpyfil, stnfil
 	LOGICAL		compress
 C-----------------------------------------------------------------------
 	CALL IP_STR  ( 'PROJ',    proj,   ier1 )
@@ -26,11 +26,10 @@ C-----------------------------------------------------------------------
 	CALL IP_STR  ( 'RADFRQ',  radfrq, ier8 )
 	CALL IP_STR  ( 'CPYFIL',  cpyfil, ier9 )
 	CALL IP_STR  ( 'STNFIL',  stnfil, ier10 )
-	CALL IP_STR  ( 'RADMODE', radmode, ier11 )
-	CALL IP_LOG  ( 'COMPRESS', compress, ier12 )
+	CALL IP_LOG  ( 'COMPRESS', compress, ier11 )
 C
 	iret =  ier1 + ier2 + ier3 + ier4 + ier5 + ier6 + ier7 + ier8 
-     +		+ ier9 + ier10 + ier11 + ier12
+     +		+ ier9 + ier10 + ier11 
 
 	IF  ( iret .ne. 0 )  iret = -2
 C*
