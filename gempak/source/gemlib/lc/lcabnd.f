792ebe34eb145b38f4aa20dfbf75eadcbdac3e70
Michael James <mjames@unidata.ucar.edu>
2014-09-02 12:44:32 -0600

    lcabnd now constructs 4-char ICAO if 3-char loc is given and not found in sfstns.tbl

--- gempak/source/gemlib/lc/lcabnd.f
+++ gempak/source/gemlib/lc/lcabnd.f
@@ -218,6 +218,7 @@ C
 C
 C*	    Check for area in the station file.
 C
+
 	    CALL LC_FSTN  ( 'sfstns.tbl', loc, cenlat, cenlon, ier )
 	    IF  ( ier .eq. 0 )  THEN
 		diflat = 4.0
@@ -234,7 +235,28 @@ C
 		iret   = 0
 	    ELSE IF  ( ier .eq. -3 )  THEN
 		iret   = -3
+	    ELSE IF  ( ier .eq. -4 )  THEN
+                IF ( LEN_TRIM(loc) .eq. 3) THEN
+                    loc = 'K' // loc
+                END IF
 	    ENDIF
+            CALL LC_FSTN  ( 'sfstns.tbl', loc, cenlat, cenlon, ier )
+            IF  ( ier .eq. 0 )  THEN
+                diflat = 4.0
+                diflon = 7.0
+                iartyp = 2
+                stn(1) = loc
+                IF ( cenlat .gt. 25. ) THEN
+                    cdproj = 'NPS'
+                ELSE IF ( cenlat .lt. -25. ) THEN
+                    cdproj = 'SPS'
+                ELSE
+                    cdproj = 'MER'
+                END IF
+                iret   = 0
+            ELSE IF  ( ier .eq. -3 )  THEN
+                iret   = -3
+            ENDIF
 	ENDIF
 C
 C*	If area has been found--compute bounds with zoom factors.
