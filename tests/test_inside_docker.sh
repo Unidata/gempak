1eb1e9a0e27145e5a2e6a39984e9205acbd64fb4
Michael James <mjames@ucar.edu>
2017-08-24 11:41:58 -0500

    fix /gempak/tests/test_inside_docker.sh: line 21: unexpected EOF while looking for matching '

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -18,8 +18,8 @@ EOF
 
 cp gempak/rpm/Installer.gempak/linux64.spec /tmp/rpmbuild/SPECS
 ln -s /home/gempak/GEMPAK7 gempak
-package_version=`grep "define version" rpm/Installer.gempak/linux64.spec | grep -v version_core| awk '{print $3}''
 pushd gempak
+package_version=`grep "define version" rpm/Installer.gempak/linux64.spec | grep -v version_core| awk '{print $3}'`
 git archive --format=tar --prefix=gempak-${package_version}/ HEAD  | gzip >/tmp/rpmbuild/SOURCES/gempak-${package_version}.tar.gz
 popd
 
