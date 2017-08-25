a652481322679a367045248e443c3e3fb4906cdf
Michael James <mjames@ucar.edu>
2017-08-25 11:32:35 -0500

    use dockerhub names as script args

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -4,20 +4,20 @@
 
 # Version of CentOS/RHEL
 os_type=$1
-el_version=$2
+os_version=$2
 
  # Run tests in Container
 if [[ "$os_type" = "centos" ]]; then
-    if [[ "$el_version" = "6" ]]; then
+    if [[ "$os_version" = "centos6" ]]; then
 
-        sudo docker run --rm=true -v `pwd`:/gempak:rw centos:centos${el_version} /bin/bash -c "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${el_version}"
+        sudo docker run --rm=true -v `pwd`:/gempak:rw ${os_type}:${os_version} /bin/bash -c "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${os_version}"
 
-    elif [[ "$el_version" = "7" ]]; then
+    elif [[ "$os_version" = "centos7" ]]; then
 
-        docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/gempak:rw  centos:centos${el_version}   /usr/sbin/init
-        DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+        docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/gempak:rw  ${os_type}:${os_version}   /usr/sbin/init
+        DOCKER_CONTAINER_ID=$(docker ps | grep ${os_type} | awk '{print $1}')
         docker logs $DOCKER_CONTAINER_ID
-        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${el_version}";
+        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${os_version}";
         docker ps -a
         docker stop $DOCKER_CONTAINER_ID
         docker rm -v $DOCKER_CONTAINER_ID
@@ -26,6 +26,6 @@ if [[ "$os_type" = "centos" ]]; then
 
 elif [[ "$os_type" = "ubuntu" ]]; then
 
-    sudo docker run --rm=true -v `pwd`:/gempak:rw ubuntu:latest /bin/bash -c "bash -xe /gempak/tests/build_ubuntu.sh "
+    sudo docker run --rm=true -v `pwd`:/gempak:rw ${os_type}:${os_version} /bin/bash -c "bash -xe /gempak/tests/build_ubuntu.sh "
 
 fi
