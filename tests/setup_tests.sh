74ca69ab02ec19e443d218877abbb91a6ec5dea3
Michael James <mjames@ucar.edu>
2017-08-25 11:23:06 -0500

    fixing arch setup for docker

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -7,25 +7,25 @@ os_type=$1
 el_version=$2
 
  # Run tests in Container
-if [ "$os_type" = "centos" ]; then
-    if [ "$el_version" = "6" ]; then
+if [[ "$os_type" = "centos" ]]; then
+    if [[ "$el_version" = "6" ]]; then
 
         sudo docker run --rm=true -v `pwd`:/gempak:rw centos:centos${el_version} /bin/bash -c "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${el_version}"
 
-    elif [ "$el_version" = "7" ]; then
+    elif [[ "$el_version" = "7" ]]; then
 
         docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/gempak:rw  centos:centos${el_version}   /usr/sbin/init
         DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
         docker logs $DOCKER_CONTAINER_ID
-        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${el_version};
+        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /gempak/tests/build_rhel.sh ${os_type} ${el_version}";
         docker ps -a
         docker stop $DOCKER_CONTAINER_ID
         docker rm -v $DOCKER_CONTAINER_ID
 
     fi
 
-elif [ "$os_type" = "ubuntu" ]; then
+elif [[ "$os_type" = "ubuntu" ]]; then
 
-    sudo docker run --rm=true -v `pwd`:/gempak:rw ubuntu:latest /bin/bash -c "bash -xe /gempak/tests/build_ubuntu.sh"
+    sudo docker run --rm=true -v `pwd`:/gempak:rw ubuntu:latest /bin/bash -c "bash -xe /gempak/tests/build_ubuntu.sh "
 
 fi
