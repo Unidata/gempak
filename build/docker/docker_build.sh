fb278117cffebf4c7623a1b21a74bcf548232e99
mjames-upc <mjames@unidata.ucar.edu>
2018-10-31 09:01:45 -0600

    7.4.4 docker build updates

--- build/docker/docker_build.sh
+++ build/docker/docker_build.sh
@@ -4,5 +4,5 @@ version=$(cat $GEMPAK/source/gemlib/ss/ssvers.f |grep "Version 7" | cut -d "'" -
 pushd $NAWIPS/build/docker
 sudo docker build -t unidata/gempak -f Dockerfile.gempak .
 dockerID=$(sudo docker images | grep gempak | grep latest | awk '{print $3}' | head -1 )
-sudo docker tag $dockerID unidata/gempak:${version}
+sudo docker tag -f $dockerID unidata/gempak:${version}
 sudo docker push unidata/gempak
