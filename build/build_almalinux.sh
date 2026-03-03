ceb80aadfb13f89c3b8953f1c5dae2a31539af08
akrherz <akrherz@iastate.edu>
2026-03-03 15:06:42 -0600

    bld: replace centos with almalinux (for now)

--- build/build_almalinux.sh
+++ build/build_almalinux.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -xe
 
-# runs on centos7, so no dnf
-yum -y -q install make gcc-gfortran git rpm-build byacc flex libX11-devel libXext libXft-devel libXp-devel libXt-devel libXtst-devel openmotif-devel xorg-x11-xbitmaps autoconf libtool automake
+dnf -y -q install make gcc-gfortran git rpm-build byacc flex libX11-devel libXext libXft-devel libXp-devel libXt-devel libXtst-devel openmotif-devel xorg-x11-xbitmaps autoconf libtool automake
 
 useradd gempak
 
--- build/setup.sh
+++ build/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -xe
-# ./setup.sh centos 7
-# ./setup.sh centos stream8
-# ./setup.sh centos stream9
+# ./setup.sh almalinux 9
+# ./setup.sh almalinux 10
 # ./setup.sh fedora latest
 # ./setup.sh ubuntu latest
 
@@ -9,19 +8,26 @@ os_type=$1
 os_version=$2
 TAG=${os_type}${os_version}
 
+if [ -z "$os_type" ] || [ -z "$os_version" ]; then
+    echo "Usage: $0 <dev|almalinux|ubuntu|fedora> <version>" >&2
+    exit 2
+fi
+
  # Run builds in Container
 
 if [ "$os_type" = "dev" ]; then
-	
-	# sudo docker run --entrypoint=/bin/bash --privileged -d -ti -e "container=docker" -v /awips2/repo/${dn}:/awips2/repo/${dn} $imgname:$imgvers-$os_version
-        sudo docker run -v `pwd`:/gempak:rw unidata/gempak-devel:centos6 /bin/bash -c "bash -xe /gempak/build/build_dev.sh"
 
-elif [ "$os_type" = "centos" ]; then
+    # sudo docker run --entrypoint=/bin/bash --privileged -d -ti -e "container=docker" -v /awips2/repo/${dn}:/awips2/repo/${dn} $imgname:$imgvers-$os_version
+    sudo docker run -v `pwd`:/gempak:rw unidata/gempak-devel:centos6 /bin/bash -c "bash -xe /gempak/build/build_dev.sh"
 
-        sudo docker run --privileged -d -ti -e "container=docker" -v `pwd`:/gempak:rw quay.io/centos/centos:$os_version /bin/bash
-        DOCKER_CONTAINER_ID=$(sudo docker ps | grep ${os_version} | awk '{print $1}' | head -1 )
+elif [ "$os_type" = "almalinux" ]; then
+
+    CONTAINER_IMAGE=quay.io/almalinuxorg/${os_version}-base
+    BUILD_SCRIPT=build_almalinux.sh
+
+    DOCKER_CONTAINER_ID=$(sudo docker run --privileged -d -ti -e "container=docker" -v `pwd`:/gempak:rw $CONTAINER_IMAGE /bin/bash)
         sudo docker logs $DOCKER_CONTAINER_ID
-        sudo docker exec --tty $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /gempak/build/build_${os_type}.sh ${os_version}";
+        sudo docker exec --tty $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /gempak/build/${BUILD_SCRIPT} ${os_version}";
         sudo docker ps -a
         sudo docker stop $DOCKER_CONTAINER_ID
         sudo docker rm -v $DOCKER_CONTAINER_ID
@@ -33,5 +39,10 @@ elif [ "$os_type" = "ubuntu" ]; then
 elif [ "$os_type" = "fedora" ]; then
 
     sudo docker run --rm=true -v `pwd`:/gempak:rw unidata/gempak-devel:$os_type /bin/bash -c "bash -xe /gempak/build/build_${os_type}.sh"
+
+else
+
+    echo "Unsupported os_type '$os_type'. Expected one of: dev, almalinux, ubuntu, fedora." >&2
+    exit 2
 
 fi
